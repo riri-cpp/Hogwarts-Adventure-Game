e11842b421f78bc7d48804dfb92efafb8d07b23d
riri-cpp <pasiarheman7@gmail.com>
2025-11-20 09:49:32 +0800

    Delete HogwartsGame directory

--- HogwartsGame/src/Main.java
+++
@@ -1,7 +0,0 @@
-// src/Main.java
-public class Main {
-    public static void main(String[] args) {
-        game.GameEngine hogwartsGame = new game.GameEngine();
-        hogwartsGame.startGame();
-    }
-}
